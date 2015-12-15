3ae763e121e8426d546b34aba0862149d930f712
Shaun Prasad <shaun.prasad1096@gmail.com>
2015-12-14 19:43:38 -0800

    Added Buttons

--- EnCryptDecrypt/frmMain.cs
+++ EnCryptDecrypt/frmMain.cs
@@ -41,5 +41,10 @@ namespace EnCryptDecrypt
             txtDecryptedText.Visible = true;
             label3.Visible = true;            
         }
+
+        private void button1_Click( object sender, EventArgs e )
+        {
+
+        }
     }
 }
